99238164751133d4d03aadbe636fbeaa531586ec
Konstantin Milonas <info@condata.io>
2024-11-29 21:26:22 +0100

    deleted postgresql login created new folder alex_the_analyst

---
+++ SQL/alex_the_analyst/create_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE employee_demographics(
+employee_id int,
+fist_name varchar(50),
+last_name varchar(50),
+age int,
+gender varchar(7)
+);
+
+CREATE TABLE employee_salary(
+employee_id int,
+jobtitel varchar(50),
+salary int);
+
