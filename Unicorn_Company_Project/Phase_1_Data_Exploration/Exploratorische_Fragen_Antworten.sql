07a33a442bbdbecb7e97a992f943a740562e79c7
Konstantin Milonas <info@condata.io>
2024-12-18 12:59:29 +0100

    feat: Update exploratory sql queries an add screen shots

--- Unicorn_Company_Project/Phase_1_Data_Exploration/Exploratorische_Fragen_Antworten.sql
+++ Unicorn_Company_Project/Phase_1_Data_Exploration/Exploratorische_Fragen_Antworten.sql
@@ -36,29 +36,15 @@ LIMIT 1;
 
 -- 3. Wie hoch war der Gewinn der profitabelsten Stadt im Jahr 2015?
 
--- Ohne CTE
+WITH cte_orders AS
+(
 SELECT
-		o.shipping_city,
-    MAX(od.order_profits) AS max_profit
-FROM orders AS o
-JOIN order_details AS od ON o.order_id = od.order_id
-JOIN cte_orders AS cto ON o.order_id = cto.order_id
-AND cto.order_year = '2015'
-GROUP BY o.shipping_city
-ORDER BY max_profit DESC
-LIMIT 1;
-
--- Mit CTE
-SELECT
-		o.shipping_city,
-    MAX(od.order_profits) AS max_profit
-FROM orders AS o
-JOIN order_details AS od ON o.order_id = od.order_id
-JOIN cte_orders AS cto ON o.order_id = cto.order_id
-AND cto.order_year = '2015'
-GROUP BY o.shipping_city
-ORDER BY max_profit DESC
-LIMIT 1;
+    SPLIT_PART(order_id, '-', 1) AS country_code, -- Teilt die order_id und extrahiert in diesem Fall den country_code.
+    SPLIT_PART(order_id, '-', 2) AS order_year,   -- Teilt die order_id und extrahiert in diesem Fall das order_year.
+    LEFT(CAST(order_date AS TEXT), 10)::DATE AS order_date, -- Extrahiert die ersten 10 Zeichen als Text und formatiert sie als Datum.
+    LEFT(CAST(shipping_date AS TEXT), 10)::DATE AS shipping_date -- Extrahiert die ersten 10 Zeichen als Text und formatiert sie anschließend als Datum.
+FROM orders
+)
 
 SELECT
 		o.shipping_city,
@@ -70,12 +56,15 @@ GROUP BY o.shipping_city
 ORDER BY max_profit DESC
 LIMIT 1;
 
+
 Ergebnis: shipping-city = Minneapolis | max_proft = 4630
 
 -- 4. Wie viele verschiedene Städte gibt es in den Daten?
 
-SELECT DISTINCT shipping_city
-FROM orders
+SELECT
+		COUNT(DISTINCT shipping_city) AS number_different_cities
+FROM orders;
+
 
 -- 5. Zeige die Gesamtausgaben der Kunden von niedrig nach hoch.
 
@@ -89,9 +78,63 @@ ORDER BY total_sales ASC;
 
 -- 6. Welche Stadt im Bundesstaat Tennessee war am profitabelsten?
 
+SELECT 
+		o.shipping_city,
+		SUM(order_profits) AS total_profits
+FROM order_details AS od
+JOIN orders AS o 
+ON od.order_id = o.order_id
+WHERE shipping_state = 'Tennessee'
+GROUP BY o.shipping_city
+ORDER BY total_profits DESC
+LIMIT 1
+
 -- 7. Wie hoch ist der durchschnittliche jährliche Gewinn für diese Stadt über alle Jahre hinweg?
 
+WITH cte_orders AS 
+(
+    SELECT
+        o.order_id,
+        SPLIT_PART(o.order_id, '-', 2) AS order_year,
+        o.shipping_city
+    FROM orders AS o
+),
+
+most_profitable_city AS 
+(
+    SELECT 
+        o.shipping_city,
+        SUM(od.order_profits) AS total_profits
+    FROM order_details AS od
+    JOIN orders AS o ON od.order_id = o.order_id
+    WHERE o.shipping_state = 'Tennessee'
+    GROUP BY o.shipping_city
+    ORDER BY total_profits DESC
+    LIMIT 1
+)
+
+SELECT 
+    cte.shipping_city,
+    cte.order_year,
+    ROUND(AVG(od.order_profits)::INT, 2) AS avg_annual_profit
+FROM order_details AS od
+JOIN orders AS o 
+		ON od.order_id = o.order_id
+JOIN cte_orders AS cte 
+		ON cte.order_id = o.order_id
+JOIN most_profitable_city AS mpc 
+		ON cte.shipping_city = mpc.shipping_city
+GROUP BY cte.order_year, cte.shipping_city
+ORDER BY cte.order_year;
+
 -- 8. Wie ist die Verteilung der Kundentypen in den Daten?
+
+SELECT 
+    customer_segment,
+    COUNT(DISTINCT customer_id) AS num_customers
+FROM customers
+GROUP BY customer_segment
+ORDER BY num_customers DESC;
 
 -- 9. Welche Produktkategorie war durchschnittlich am profitabelsten in Iowa über alle Jahre hinweg?
 
--- Unicorn_Company_Project/Phase_1_Data_Exploration/Exploratory_Questions_Answers.sql
+++ Unicorn_Company_Project/Phase_1_Data_Exploration/Exploratory_Questions_Answers.sql
@@ -4,15 +4,7 @@
 
 -- To make the queries clearer, I first created a CTE (Common Table Expression) for the following table:
 -- orders:
-WITH cte_orders AS
-(
-SELECT
-    SPLIT_PART(order_id, '-', 1) AS country_code, -- Splits the order_id and extracts the country_code in this case
-    SPLIT_PART(order_id, '-', 2) AS order_year,   -- Splits the order_id and extracts the order_year in this case
-    LEFT(CAST(order_date AS TEXT), 10)::DATE AS order_date, -- Extracts the first 10 characters as text and formats it as a date
-    LEFT(CAST(shipping_date AS TEXT), 10)::DATE AS shipping_date -- Extracts the first 10 characters as text and formats it as a date
-FROM orders
-)
+
 
 -- The CTE "cte_orders" can be accessed like a table. Since it is cached after the first execution,
 -- retrieval is very fast.
@@ -36,29 +28,15 @@ LIMIT 1;
 
 -- 3. In 2015, what was the most profitable city’s profit?
 
--- Without CTE
+WITH cte_orders AS
+(
 SELECT
-		o.shipping_city,
-    MAX(od.order_profits) AS max_profit
-FROM orders AS o
-JOIN order_details AS od ON o.order_id = od.order_id
-JOIN cte_orders AS cto ON o.order_id = cto.order_id
-AND cto.order_year = '2015'
-GROUP BY o.shipping_city
-ORDER BY max_profit DESC
-LIMIT 1;
-
--- With CTE
-SELECT
-		o.shipping_city,
-    MAX(od.order_profits) AS max_profit
-FROM orders AS o
-JOIN order_details AS od ON o.order_id = od.order_id
-JOIN cte_orders AS cto ON o.order_id = cto.order_id
-AND cto.order_year = '2015'
-GROUP BY o.shipping_city
-ORDER BY max_profit DESC
-LIMIT 1;
+    SPLIT_PART(order_id, '-', 1) AS country_code, -- Splits the order_id and extracts the country_code in this case
+    SPLIT_PART(order_id, '-', 2) AS order_year,   -- Splits the order_id and extracts the order_year in this case
+    LEFT(CAST(order_date AS TEXT), 10)::DATE AS order_date, -- Extracts the first 10 characters as text and formats it as a date
+    LEFT(CAST(shipping_date AS TEXT), 10)::DATE AS shipping_date -- Extracts the first 10 characters as text and formats it as a date
+FROM orders
+)
 
 SELECT
 		o.shipping_city,
@@ -70,26 +48,84 @@ GROUP BY o.shipping_city
 ORDER BY max_profit DESC
 LIMIT 1;
 
+
 -- 4. How many different cities do we have in the data?
 
-SELECT DISTINCT shipping_city
-FROM orders
+SELECT
+		COUNT(DISTINCT shipping_city) AS number_different_cities
+FROM orders;
+
+
 
 -- 5. Show the total spent by customers from low to high.
 
 SELECT
 		o.customer_id AS customers,
-		SUM(order_sales) AS total_sales
+		SUM(order_sales) AS total_spend
 FROM orders AS o
 JOIN order_details AS od ON o.order_id = od.order_id
 GROUP BY o.customer_id
-ORDER BY total_sales ASC;
+ORDER BY total_spend DESC;
 
 -- 6. What is the most profitable city in the State of Tennessee?
 
+SELECT 
+		o.shipping_city,
+		SUM(order_profits) AS total_profits
+FROM order_details AS od
+JOIN orders AS o 
+ON od.order_id = o.order_id
+WHERE shipping_state = 'Tennessee'
+GROUP BY o.shipping_city
+ORDER BY total_profits DESC
+LIMIT 1
+
 -- 7. What’s the average annual profit for that city across all years?
 
+WITH cte_orders AS 
+(
+    SELECT
+        o.order_id,
+        SPLIT_PART(o.order_id, '-', 2) AS order_year,
+        o.shipping_city
+    FROM orders AS o
+),
+
+most_profitable_city AS 
+(
+    SELECT 
+        o.shipping_city,
+        SUM(od.order_profits) AS total_profits
+    FROM order_details AS od
+    JOIN orders AS o ON od.order_id = o.order_id
+    WHERE o.shipping_state = 'Tennessee'
+    GROUP BY o.shipping_city
+    ORDER BY total_profits DESC
+    LIMIT 1
+)
+
+SELECT 
+    cte.shipping_city,
+    cte.order_year,
+    ROUND(AVG(od.order_profits)::INT, 2) AS avg_annual_profit
+FROM order_details AS od
+JOIN orders AS o 
+		ON od.order_id = o.order_id
+JOIN cte_orders AS cte 
+		ON cte.order_id = o.order_id
+JOIN most_profitable_city AS mpc 
+		ON cte.shipping_city = mpc.shipping_city
+GROUP BY cte.order_year, cte.shipping_city
+ORDER BY cte.order_year;
+
 -- 8. What is the distribution of customer types in the data?
+
+SELECT 
+    customer_segment,
+    COUNT(DISTINCT customer_id) AS num_customers
+FROM customers
+GROUP BY customer_segment
+ORDER BY num_customers DESC;
 
 -- 9. What’s the most profitable product category on average in Iowa across all years?
 
