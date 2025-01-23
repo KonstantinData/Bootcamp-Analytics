4bbfb14c676b48f6c3eac18fd2c1699695da71e6
Konstantin Milonas <info@condata.io>
2025-01-23 12:05:21 +0100

    feat: Start over with new Data Structure for Python

--- Python/Pandas/.ipynb_checkpoints/ReadingFiles-checkpoint.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-
-pd.read_excel(r"D:\Repositories\Git_GitHub\PandasAlexTheAnalyst\Customer Call List.xlsx")
--- Python/read_csv.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df =pd.read_excel("C:\\Users\\konst\\Downloads\\dataset.xlsx",sheet_name=0,header=0)
-
-print(df.tail(3))
-
-df.info()
